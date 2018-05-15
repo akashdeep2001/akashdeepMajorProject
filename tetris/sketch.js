d439ace5206d68ae8daa17263b67e436bda2176b
Singh3802389 <3802389@sps-student.sk.ca>
2018-05-15 15:27:22 -0600

    initial commit

---
+++ tetris/sketch.js
@@ -0,0 +1,23 @@
+///TETRIS///
+let grid = [];
+
+function setup(){
+  grid = gridOne();
+}
+
+function draw(){
+
+}
+
+function tiles(){
+
+
+}
+
+function gridOne(){
+  for(let i = 0; i<height; i++){
+    for(let j = 0; j<width; j++){
+      \
+    }
+  }
+}
