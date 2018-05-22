37b5d0347a94ac492daa1263ef898a31fdc6a291
Singh3802389 <3802389@sps-student.sk.ca>
2018-05-22 15:20:20 -0600

    22 may

--- tetris/sketch.js
+++ tetris/sketch.js
@@ -1,23 +1,26 @@
 ///TETRIS///
-let grid = [];
-
-function setup(){
-  grid = gridOne();
+let canWidth;
+let canHeight;
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  background(0);
+  canWidth = 300;
+  canHeight = 600;
 }
 
 function draw(){
 
+  rect(windowWidth/2-canWidth/2,windowHeight/2-canHeight/2,canWidth,canHeight);
+  move();
+
+
 }
 
-function tiles(){
-
-
-}
-
-function gridOne(){
-  for(let i = 0; i<height; i++){
-    for(let j = 0; j<width; j++){
-      \
-    }
+function move(){
+  let x = 20;
+  let y = 40;
+  rect(windowWidth/2, windowHeight/2, x,y);
+  if(y<200){
+    y = y+1;
   }
 }
