3a758da83c5f1a4e862ebf78edb86eccb33ab2a3
Singh3802389 <3802389@sps-student.sk.ca>
2018-06-05 14:32:24 -0600

    first

--- tetris/sketch.js
+++ tetris/sketch.js
@@ -1,26 +1,77 @@
-///TETRIS///
-let canWidth;
-let canHeight;
-function setup() {
-  createCanvas(windowWidth,windowHeight);
-  background(0);
-  canWidth = 300;
-  canHeight = 600;
+let player;
+let groud = [];
+let blocks = ["blue", "red, green", "brown", "yellow", "orange", "purple"]; // color of each array for the blocks
+
+function setup(){
+  createCanvas(240,400);
 }
 
 function draw(){
-
-  rect(windowWidth/2-canWidth/2,windowHeight/2-canHeight/2,canWidth,canHeight);
-  move();
-
-
+  background(150); // grey
 }
 
-function move(){
-  let x = 20;
-  let y = 40;
-  rect(windowWidth/2, windowHeight/2, x,y);
-  if(y<200){
-    y = y+1;
+function tile(type){  // get block array
+  switch (type) {     //this function willl return the shape of the blocks
+  case 0:
+    return [
+      [1, 1],
+      [1, 1],
+    ];
+
+  case 1:
+    return [
+      [2, 2, 0],
+      [0, 2, 2],
+      [0, 0, 0],
+    ];
+
+  case 2:
+    return [
+      [0, 3, 3],
+      [3, 3, 0],
+      [0, 0, 0],
+    ];
+
+  case 3:
+    return [
+      [0, 0, 0],
+      [4, 4, 4],
+      [0, 4, 0],
+    ];
+
+  case 4:
+    return [
+      [0, 5, 0],
+      [0, 5, 0],
+      [0, 5, 5],
+    ];
+
+  case 5:
+    return [
+      [0, 6, 0],
+      [0, 6, 0],
+      [6, 6, 0],
+    ];
+
+  case 6:
+    return [
+      [0, 7, 0, 0],
+      [0, 7, 0, 0],
+      [0, 7, 0, 0],
+      [0, 7, 0, 0],
+    ];
+
+  }
+}
+
+function keyPressed(){
+  function keyPressed() {
+    if (keyCode === LEFT_ARROW) {
+      player.x--;                         // move block left
+    }
+    else if (keyCode === RIGHT_ARROW) {
+      player.x++;                         // move block left
+
+    }
   }
 }
